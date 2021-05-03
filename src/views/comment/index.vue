d4a0071e5729ee5712fca3e117dbccb8d6b3374d
samele <angelsdemons248@sina.com>
2021-05-04 01:55:29 +0800

    个人设置完成一半

--- src/views/comment/index.vue
+++ src/views/comment/index.vue
@@ -1,21 +1,127 @@
 <template>
     <div class="comment-container">
-        评论管理
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>卡片名称</span>
+            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+          </div>
+          <el-table
+            :data="articles" stripe
+            style="width: 100%">
+            <el-table-column
+              prop="title"
+              label="标题"
+              align="center"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              prop="total_comment_count"
+              label="总评论数"
+              align="center"
+              width="180">
+            </el-table-column>
+            <el-table-column
+              prop="fans_comment_count"
+              align="center"
+              label="粉丝评论数">
+            </el-table-column>
+            <el-table-column
+              prop="comment_status"
+              align="center"
+              label="评论状态">
+              <template slot-scope="scope">
+                {{ scope.row.comment_status ? '正常' : '关闭' }}
+              </template>
+            </el-table-column>
+            <el-table-column
+              prop="address"
+              align="center"
+              label="操作">
+              <template slot-scope="scope">
+                <el-switch
+                  style="display: block"
+                  v-model="scope.row.comment_status"
+                  :disabled = "scope.row.statusDisabled"
+                  active-color="#13ce66"
+                  inactive-color="#ff4949"
+                  active-text="按月付费"
+                  inactive-text="按年付费" @change="onStatusChange( scope.row )">
+                </el-switch>
+              </template>
+            </el-table-column>
+          </el-table>
+          <!-- 分页 -->
+          <el-pagination
+            @size-change="handleSizeChange"
+            @current-change="handleCurrentChange"
+            :current-page.sync="page"
+            :page-sizes="[10, 20, 50]"
+            :page-size.sync="pageSize"
+            layout="total, sizes, prev, pager, next, jumper"
+            background
+            :total="totalCount"/>
+        </el-card>
     </div>
 </template>
 <script>
-// import 代称 from '地址'
+import { getArticles, updateCommentStatus } from '@/api/article'
 export default {
   name: 'Comment',
   components: {},
   props: {},
   data () {
-    return { }
+    return {
+      articles: [], // 接收数据
+      totalCount: 0, // 总数据条数
+      pageSize: 10,
+      page: 1
+    }
   },
   computed: {},
-  created () {},
+  created () {
+    this.loadArticles()
+  },
   mounted () {},
-  methods: { },
+  methods: {
+    // 多少条数据每页
+    handleSizeChange (val) {
+      this.loadArticles(1)
+      console.log('是啊', val)
+    },
+    // 页码改变，加载指定页码数据
+    handleCurrentChange (val) {
+      this.loadArticles(val)
+      console.log(`当前页: ${val}`)
+    },
+    // 初始化页面
+    loadArticles (page) {
+      this.page = page
+      getArticles({
+        response_type: 'comment',
+        page,
+        per_page: this.pageSize
+      }).then(({ data: { data } }) => {
+        console.log('评论管理', data)
+        data.results.forEach(article => {
+          article.statusDisabled = false
+        })
+        this.articles = data.results
+        this.totalCount = data.total_count
+      })
+    },
+    onStatusChange (article) {
+      article.statusDisabled = true
+      console.log('评论状态', article)
+      updateCommentStatus(article.id.toString(), article.comment_status).then(res => {
+        console.log('评论状态的返回', res)
+        article.statusDisabled = false
+        this.$message({
+          type: 'success',
+          message: article.comment_status ? '开启成功' : '关闭成功'
+        })
+      })
+    }
+  },
   watch: {}
 }
 </script>
--- src/views/settings/index.vue
+++ src/views/settings/index.vue
@@ -1,21 +1,101 @@
 <template>
     <div class="settings-container">
-        个人设置
+        <el-card class="box-card">
+          <div slot="header" class="clearfix">
+            <span>面包屑</span>
+            <el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+          </div>
+          <el-row :gutter="20">
+            <el-col :span="16">
+              <el-form ref="form" :model="user" label-width="100px">
+                <el-form-item label="编号：">
+                  {{ user.id }}
+                </el-form-item>
+                <el-form-item label="手机：">
+                  {{ user.mobile }}
+                </el-form-item>
+                <el-form-item label="媒体名称：">
+                  <el-input v-model="user.name"></el-input>
+                </el-form-item>
+                <el-form-item label="媒体介绍：">
+                  <el-input type="textarea" v-model="user.intro"></el-input>
+                </el-form-item>
+                <el-form-item label="邮箱：">
+                  <el-input v-model="user.email"></el-input>
+                </el-form-item>
+                <el-form-item>
+                  <el-button type="primary" @click="onSubmit">保存</el-button>
+                </el-form-item>
+              </el-form>
+             </el-col>
+              <el-col :span="6" :offset="2">
+                <label for="file">
+                  <el-avatar
+                    shape="square"
+                    :size="160"
+                    fit="cover"
+                    :src="user.photo"/>
+                    <p>点击修改头像</p>
+                </label>
+                <input id="file" type="file" hidden ref="file" @change="onFileChange"/>
+              </el-col>
+          </el-row>
+        </el-card>
+        <el-dialog
+          title="修改头像"
+          :visible.sync="dialogVisible"
+          append-to-body>
+          <img width="150" :src="previewImage" alt="">
+          <span slot="footer" class="dialog-footer">
+         <el-button @click="dialogVisible = false">取 消</el-button>
+    <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+  </span>
+</el-dialog>
     </div>
 </template>
 <script>
-// import 代称 from '地址'
+import { getUserProfile } from '@/api/user'
 export default {
   name: 'Settings',
   components: {},
   props: {},
   data () {
-    return { }
+    return {
+      user: {
+        email: '',
+        id: null,
+        intro: '',
+        mobile: '',
+        name: '',
+        photo: ''
+      },
+      dialogVisible: false, // 控制上传图片预览显示状态
+      previewImage: '' // 预览的图片
+    }
   },
   computed: {},
-  created () {},
+  created () {
+    this.loadUser()
+  },
   mounted () {},
-  methods: { },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    },
+    loadUser () {
+      getUserProfile().then(res => {
+        this.user = res.data.data
+        console.log('当前用户信息', res)
+      })
+    },
+    onFileChange () {
+      const file = this.$refs.file
+      const blobData = window.URL.createObjectURL(file.files[0])
+      this.previewImage = blobData
+      this.dialogVisible = true
+      this.$refs.file.value = ''
+    }
+  },
   watch: {}
 }
 </script>
